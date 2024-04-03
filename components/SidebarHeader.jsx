2418a45d0fd418f3290aab6610b3b6f0eeddc683
Muhammad Gamal <gamal9978@gmail.com>
2024-04-03 06:39:06 +0200

    added nav link

--- components/SidebarHeader.jsx
+++ components/SidebarHeader.jsx
@@ -1,4 +1,12 @@
+import { RiOpenaiFill } from "react-icons/ri";
+import ThemeToggle from "./ThemeToggle";
 const SidebarHeader = () => {
-  return <div>SidebarHeader</div>;
+  return (
+    <div className="flex items-center mb-4 gap-4 px-4">
+      <RiOpenaiFill className="h-10 w-10 text-primary" />
+      <h2 className="text-xl font-extrabold text-primary">ChatGPT</h2>
+      <ThemeToggle />
+    </div>
+  );
 };
 export default SidebarHeader;
